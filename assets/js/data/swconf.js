605aa3df7d2b58182920e7660c6cbcb512dbe7ea
min75046591 <min75046591@users.noreply.github.com>
2024-12-24 18:53:37 +0000

    deploy: 0234ba8b6eb8e35df5d8f4b53def7e1666155210

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735066214',resources: [
+    cacheName: 'chirpy-1735066416',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
